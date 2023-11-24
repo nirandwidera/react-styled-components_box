29d6e414d6997bc41f2be4a2dde2af80f045ca5a
Nirand Widera <nirand-widera@web.de>
2023-11-24 12:46:37 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@ import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js
 import BoxWithStyledComponents from "@/components/BoxWithStyledComponents.js";
 import styled from "styled-components";
 
-const div = styled.div`
-display: flex;
+
+const FlexWrapper = styled.div`
+  display: flex;
 `;
 
 export default function HomePage() {
   return (
-    <div>
+    <FlexWrapper>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-      <BoxWithStyledComponents $isBlack/>
-      <BoxWithStyledComponents $isBlack={false}/>
-    </div>
+      <BoxWithStyledComponents $isBlack />
+      <BoxWithStyledComponents $isBlack={false} />
+    </FlexWrapper>
   );
 }
